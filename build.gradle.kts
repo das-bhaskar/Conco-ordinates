f652d143131f713af90b0701fa6c02e654b9f681
Robert <robertc1612@gmail.com>
2026-02-04 19:35:39 -0500

    Integrate SonarQube for code quality analysis
    Add SonarQube plugin and configuration properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.secrets.gradle) apply false
+    id("org.sonarqube") version "7.1.0.6387"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "soen-390-the-irs_backend")
+        property("sonar.organization", "soen-390-the-irs")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }

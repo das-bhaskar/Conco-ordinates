13c89217b9ae74ab5fb4ccf3b8b8aabc5d0bcf49
Robert <robertc1612@gmail.com>
2026-02-04 19:28:10 -0500

    Remove Kotlin scripting compiler dependencies
    Removed Kotlin scripting compiler dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,9 +66,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
-    implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:1.9.22")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-impl-embeddable:1.9.22")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-common:1.9.22")
+    
 
 
     // Maps dependency

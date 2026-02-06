8c995290210940eed7fc9e3c0e6044b80290506a
Robert <robertc1612@gmail.com>
2026-02-05 22:32:56 -0500

    Add SonarCloud properties to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,10 @@ sonar {
   properties {
     property("sonar.projectKey", "soen-390-the-irs_backend")
     property("sonar.organization", "soen-390-the-irs")
+    property("sonar.host.url", "https://sonarcloud.io")
+    property("sonar.sources", "app/src/main")
+    property("sonar.tests", "app/src/test")
+    property("sonar.java.binaries", "app/build/intermediates/javac")
+    property("sonar.android.lint.report", "app/build/reports/lint-results.xml")
   }
 }

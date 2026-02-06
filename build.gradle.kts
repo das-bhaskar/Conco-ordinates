1d9d7f72871b52dcc0fe1a9e566870a858790d17
Robert <robertc1612@gmail.com>
2026-02-05 22:59:35 -0500

    Update SonarQube plugin version to 4.4.1.3373

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.secrets.gradle) apply false
-    id("org.sonarqube") version "7.1.0.6387" apply false
+    id("org.sonarqube") version "4.4.1.3373" apply false
 }
 

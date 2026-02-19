013fc328717d3b608322b81df3124035fcc41796
Hildthelsta <d_cazac@live.concordia.ca>
2026-02-18 21:02:45 -0500

    Added Tests
    - Added Test for getCenterLatLng
    - Added Test for getPanorama

--- app/src/test/java/com/example/myapplication/MapManagerTest.kt
+++ app/src/test/java/com/example/myapplication/MapManagerTest.kt
@@ -61,6 +61,39 @@ class MapManagerTest {
         assertEquals(LatLng(45.4972712, -73.5789185), result)
     }
 
+    @Test
+    fun `test getCenterLatLng for a building without wayID`() = runTest() {
+        var building = Building(
+            "Henry F. Hall Building",
+            "H",
+            0,
+            "1455 De Maisonneuve Blvd. W., Montreal, Quebec",
+            listOf(
+                LatLng(45.4968261, -73.5788241),
+                LatLng(45.4970373, -73.5786245),
+                LatLng(45.4972553, -73.5784091),
+                LatLng(45.4973713, -73.5782939),
+                LatLng(45.4974226, -73.5783991),
+                LatLng(45.4975103, -73.5785786),
+                LatLng(45.4977164, -73.5790075),
+                LatLng(45.4977130, -73.5790121),
+                LatLng(45.4974475, -73.5792690),
+                LatLng(45.4971739, -73.5795378),
+                LatLng(45.4971671, -73.5795431),
+                LatLng(45.4971280, -73.5794591),
+                LatLng(45.4968261, -73.5788241)
+            )
+        )
+        var result = mapManager.getCenterLatLng(building)
+        assertEquals(LatLng(0.0, 0.0), result)
+    }
+
+    @Test
+    fun `test getPanorama for false coordinates when getCenterLatLng doesn't return true`() = runTest() {
+        var result = mapManager.getPanorama(LatLng(0.0, 0.0))
+        assertEquals(null, result)
+    }
+
     fun `test findBuildingAtLocation when user is inside building`() {
         val hallOutline = listOf(
             LatLng(45.4968, -73.5788),

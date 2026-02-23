88a5d23236bfffd663d44d982a5faf4afef92011
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2026-02-23 00:28:08 -0500

    Update Route.kt

--- app/src/main/java/com/example/myapplication/logic/Route.kt
+++ app/src/main/java/com/example/myapplication/logic/Route.kt
@@ -8,12 +8,6 @@ import java.io.IOException
 import okhttp3.*
 import org.json.JSONObject
 
-enum class TravelMode {
-    PUB_TRANSIT,
-    MOTORIZED,
-    WALK,
-}
-
 enum class RouteUpdateStatus {
     SUCCESS,
     MISSING_TRAVEL_MODE,
@@ -21,7 +15,7 @@ enum class RouteUpdateStatus {
     MISSING_END,
 }
 
-class Route(private val context: Context) {
+class Route(private var routeProvider: RouteProvider) {
     private var route: List<LatLng>? = null
     private var travelMode: TravelMode? = null
     private var start: LatLng? = null
@@ -59,64 +53,13 @@ class Route(private val context: Context) {
         val endIm: LatLng = end ?: return RouteUpdateStatus.MISSING_END
         val travelModeIm: TravelMode = travelMode ?: return RouteUpdateStatus.MISSING_TRAVEL_MODE
         val callbackIm = callback
-        reqPath(startIm, endIm, travelModeIm, callbackIm)
+        routeProvider.getRoute(
+            startIm,
+            endIm,
+            travelModeIm,
+            callbackIm
+        )
 
         return RouteUpdateStatus.SUCCESS
-    }
-
-    // Send to callback function a path between two points
-    private fun reqPath(start: LatLng, end: LatLng, travelMode: TravelMode, callback: (List<LatLng>) -> Unit) {
-        val mode = when (travelMode) {
-            TravelMode.PUB_TRANSIT -> "transit"
-            TravelMode.MOTORIZED -> "driving"
-            TravelMode.WALK -> "walking"
-        }
-
-        val appInfo = context.packageManager.getApplicationInfo(context.packageName, PackageManager.GET_META_DATA)
-        val apiKey = appInfo.metaData.getString("com.google.android.geo.API_KEY")
-
-        val url = "https://maps.googleapis.com/maps/api/directions/json?" +
-                "origin=${start.latitude},${start.longitude}" +
-                "&destination=${end.latitude},${end.longitude}" +
-                "&mode=$mode" +
-                "&key=${apiKey}"
-
-        val client = OkHttpClient()
-        val request = Request.Builder().url(url).build()
-
-        // Requests route from google api
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                response.body.string().let { json ->
-                    val points = decodeRoute(json)
-                    callback(points)
-                }
-            }
-        })
-    }
-
-    // Returns parsed path given a valid json string and exception otherwise.
-    private fun decodeRoute(json: String): List<LatLng> {
-        val points = mutableListOf<LatLng>()
-        try {
-            val jsonObject = JSONObject(json)
-            val routes = jsonObject.getJSONArray("routes")
-            if (routes.length() > 0) {
-                val lineBytes = routes.getJSONObject(0)
-                    .getJSONObject("overview_polyline")
-                    .getString("points")
-                // Google api returns string of characters representing bytes
-                // describing the points in a sequence. These need to be parsed
-                // with a known algorithm before usable.
-                points.addAll(PolyUtil.decode(lineBytes))
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return points
     }
 }

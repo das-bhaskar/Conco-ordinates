569aaf3a00371279a4f9057b4bf868cd135e52f1
Robert <robertc1612@gmail.com>
2026-03-04 19:28:16 -0500

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
@@ -8,5 +5,3 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
 }
-
-

2ddea0772a4037587f790d2421843315f49b0d1a
Robert <robertc1612@gmail.com>
2026-03-08 21:00:37 -0400

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,29 +7,6 @@ plugins {
     alias(libs.plugins.secrets.gradle) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
-    alias(libs.plugins.sonarqube)
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "soen-390-the-irs_backend")
-        property("sonar.organization", "soen-390-the-irs")
-        property("sonar.host.url", "https://sonarcloud.io")
-
-        property("sonar.sources", "app/src/main/java")
-        property("sonar.tests", "app/src/test/java")
-        property("sonar.sourceEncoding", "UTF-8")
-
-        property("sonar.java.binaries",
-            "app/build/intermediates/built_in_kotlinc/debug/compileDebugKotlin/classes"
-        )
-
-        property("sonar.coverage.jacoco.xmlReportPaths",
-            "app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
-        )
-
-        property("sonar.kotlin.source.version", "2.0")
-    }
 }
 
 

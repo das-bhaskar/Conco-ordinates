7f90fd708e0bd524d0a09d8b5a8786b4c644f204
bhumi-0902 <149818633+bhumi-0902@users.noreply.github.com>
2026-03-27 02:29:38 -0400

    Remove unused flow entries from run_e2e.sh
    Removed several flow entries from the FLOWS array.

--- scripts/run_e2e.sh
+++ scripts/run_e2e.sh
@@ -6,14 +6,7 @@ TEST_OUTPUT_DIR="${TEST_OUTPUT_DIR:-e2e-test-output}"
 
 FLOWS=(
   "us_1_1"
-  "us_1_2"
-  "us_1_3"
-  "us_1_5"
-  "us_1_6"
-  "us_2_1"
-  "us_2_3"
-  "us_2_5"
-  "us_2_6"
+ 
 )
 
 mkdir -p "$REPORT_DIR"
